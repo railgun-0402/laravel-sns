91dfeb4185c80508effc859750495f092705324e
railgun-0402 <april-life.f-441-2011@docomo.ne.jp>
2023-07-05 19:45:41 +0900

    add register feature

--- laravel/resources/views/auth/register.blade.php
+++ laravel/resources/views/auth/register.blade.php
@@ -1,74 +1,49 @@
-@extends('layouts.app')
+@extends('app')
+
+@section('title', 'ユーザー登録')
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
+    <div class="row">
+        <div class="mx-auto col col-12 col-sm-11 col-md-9 col-lg-7 col-xl-6">
+            <h1 class="text-center"><a class="text-dark" href="/">memo</a></h1>
+            <div class="card mt-3">
+                <div class="card-body text-center">
+                    <h2 class="h3 card-title text-center mt-2">ユーザー登録</h2>
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('register') }}">
-                        @csrf
+                    @include('error_card_list')
 
-                        <div class="form-group row">
-                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
-
-                                @error('name')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
+                    <div class="card-text">
+                        <form method="POST" action="{{ route('register') }}">
+                            @csrf
+                            <div class="md-form">
+                                <label for="name">ユーザー名</label>
+                                <input class="form-control" type="text" id="name" name="name" required
+                                    value="{{ old('name') }}">
+                                <small>英数字3〜16文字(登録後の変更はできません)</small>
                             </div>
+                            <div class="md-form">
+                                <label for="email">メールアドレス</label>
+                                <input class="form-control" type="text" id="email" name="email" required
+                                    value="{{ old('email') }}">
+                            </div>
+                            <div class="md-form">
+                                <label for="password">パスワード</label>
+                                <input class="form-control" type="password" id="password" name="password" required>
+                            </div>
+                            <div class="md-form">
+                                <label for="password_confirmation">パスワード(確認)</label>
+                                <input class="form-control" type="password" id="password_confirmation"
+                                    name="password_confirmation" required>
+                            </div>
+                            <button class="btn btn-block blue-gradient mt-2 mb-2" type="submit">ユーザー登録</button>
+                        </form>
+
+                        <div class="mt-0">
+                            <a href="{{ route('login') }}" class="card-text">ログインはこちら</a>
                         </div>
 
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Register') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
+                    </div>
                 </div>
             </div>
         </div>
--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -2,4 +2,5 @@
 
 use Illuminate\Support\Facades\Route;
 
+Auth::routes();
 Route::get('/', 'ArticleController@index');
